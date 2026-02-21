1aa7f93fddd68d7932fb5666845879bd1ebb8005
RituAddepalli <rituaddepalli@gmail.com>
2026-02-21 20:06:55 +0530

    Update Postman collection with live Render URL

--- app.py
+++ app.py
@@ -117,7 +117,13 @@ def home():
 # =====================================================
 # RUN APP
 # =====================================================
+# if __name__ == "__main__":
+#     print("Starting Resume Skill Matcher API...")
+#     app.run(debug=True, port=5000)
+import os
+
 if __name__ == "__main__":
     print("Starting Resume Skill Matcher API...")
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
